d86e5e651d360a8995ac727507145a3f77ab48bf
BALASURYA-D <suryadbala@gmail.com>
2025-07-15 10:37:37 +0530

    smallchange

--- app/layout.js
+++ app/layout.js
@@ -3,10 +3,13 @@ import './globals.css';
 
 export default function RootLayout({ children }) {
   return (
-    <>
+    <html lang="en">
+      <body>
+      
+        <Navbar></Navbar>
       
         {children}
-      
-    </>
+          </body>
+      </html>
   );
 }
